d4a27a118088c20faf74877657536d87c7d45401
casainho <casainho@a02c272c-b14d-0410-965c-4f7f0f620247>
2008-06-07 09:20:34 +0000

    first firmware version

---
+++ trunk/BicycleLEDPOV/BicycleLEDPOV.c
@@ -0,0 +1,232 @@
+/*
+             Bicycle LED POV
+     Copyright (C) Jorge Pinto aka Casainho, 2008.
+              
+  casainho [at] gmail [dot] com
+      www.casainho.net
+
+ Released under the GPL Licence, Version 3       
+*/
+
+/*
+             MyUSB Library
+     Copyright (C) Dean Camera, 2008.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+
+ Released under the LGPL Licence, Version 3
+*/
+
+/*
+	Communications Device Class demonstration application.
+	This gives a simple reference application for implementing
+	a USB to Serial converter. Sent and recieved data on the
+	serial port is communicated to the USB host.
+	
+	Before running, you will need to install the INF file that
+	is located in the CDC project directory. This will enable
+	Windows to use its inbuilt CDC drivers, negating the need
+	for special Windows drivers for the device. To install,
+	right-click the .INF file and choose the Install option.
+*/
+
+/*
+	USB Mode:           Device
+	USB Class:          Communications Device Class (CDC)
+	USB Subclass:       Abstract Control Model (ACM)
+	Relevant Standards: USBIF CDC Class Standard
+	Usable Speeds:      Full Speed Mode
+*/
+
+#include "BicycleLEDPOV.h"
+
+/* Scheduler Task List */
+TASK_LIST
+{
+	{ Task: USB_USBTask          , TaskStatus: TASK_STOP },
+	{ Task: CDC_Task             , TaskStatus: TASK_STOP },
+};
+
+/* Globals: */
+CDC_Line_Coding_t LineCoding = { BaudRateBPS: 9600,
+                                 CharFormat:  OneStopBit,
+                                 ParityType:  Parity_None,
+                                 DataBits:    8            };
+
+int main(void)
+{
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	/* Disable Clock Division */
+	SetSystemClockPrescaler(0);
+
+	/* Hardware Initialization */
+	Hardware_Init();
+
+	/* Initialize Scheduler so that it can be used */
+	Scheduler_Init();
+
+	/* Initialize USB Subsystem */
+	USB_Init();
+
+	/* Scheduling - routine never returns, so put this last in the main function */
+	Scheduler_Start();
+}
+
+EVENT_HANDLER(USB_Connect)
+{
+	/* Start USB management task */
+	Scheduler_SetTaskMode(USB_USBTask, TASK_RUN);
+}
+
+EVENT_HANDLER(USB_Disconnect)
+{
+	/* Stop running CDC and USB management tasks */
+	Scheduler_SetTaskMode(CDC_Task, TASK_STOP);
+	Scheduler_SetTaskMode(USB_USBTask, TASK_STOP);
+}
+
+EVENT_HANDLER(USB_ConfigurationChanged)
+{
+	/* Setup CDC Notification, Rx and Tx Endpoints */
+	Endpoint_ConfigureEndpoint(CDC_NOTIFICATION_EPNUM, EP_TYPE_INTERRUPT,
+		                       ENDPOINT_DIR_IN, CDC_NOTIFICATION_EPSIZE,
+	                           ENDPOINT_BANK_SINGLE);
+
+	Endpoint_ConfigureEndpoint(CDC_TX_EPNUM, EP_TYPE_BULK,
+		                       ENDPOINT_DIR_IN, CDC_TXRX_EPSIZE,
+	                           ENDPOINT_BANK_DOUBLE);
+
+	Endpoint_ConfigureEndpoint(CDC_RX_EPNUM, EP_TYPE_BULK,
+		                       ENDPOINT_DIR_OUT, CDC_TXRX_EPSIZE,
+	                           ENDPOINT_BANK_DOUBLE);
+
+	/* Start CDC task */
+	Scheduler_SetTaskMode(CDC_Task, TASK_RUN);
+}
+
+EVENT_HANDLER(USB_UnhandledControlPacket)
+{
+	uint8_t* LineCodingData = (uint8_t*)&LineCoding;
+
+	Endpoint_Ignore_Word();
+
+	/* Process CDC specific control requests */
+	switch (Request)
+	{
+		case GET_LINE_CODING:
+			if (RequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+			{
+				Endpoint_ClearSetupReceived();
+
+				for (uint8_t i = 0; i < sizeof(LineCoding); i++)
+				  Endpoint_Write_Byte(*(LineCodingData++));	
+				
+				Endpoint_ClearSetupIN();
+				while (!(Endpoint_IsSetupINReady()));
+				
+				while (!(Endpoint_IsSetupOUTReceived()));
+				Endpoint_ClearSetupOUT();
+			}
+			
+			break;
+		case SET_LINE_CODING:
+			if (RequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+			{
+				Endpoint_ClearSetupReceived();
+
+				while (!(Endpoint_IsSetupOUTReceived()));
+
+				for (uint8_t i = 0; i < sizeof(LineCoding); i++)
+				  *(LineCodingData++) = Endpoint_Read_Byte();
+
+				Endpoint_ClearSetupOUT();
+
+				Endpoint_ClearSetupIN();
+				while (!(Endpoint_IsSetupINReady()));
+			}
+	
+			break;
+		case SET_CONTROL_LINE_STATE:
+			if (RequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+			{
+				Endpoint_ClearSetupReceived();
+				
+				Endpoint_ClearSetupIN();
+				while (!(Endpoint_IsSetupINReady()));
+			}
+	
+			break;
+	}
+}
+
+TASK(CDC_Task)
+{
+	unsigned char ucData = 0;	
+		
+	if (USB_IsConnected)
+	{
+		/* Select the Serial Rx Endpoint */
+		Endpoint_SelectEndpoint(CDC_RX_EPNUM);
+		
+		if (Endpoint_ReadWriteAllowed())
+		{
+			/* Read the recieved data endpoint into the transmission buffer */
+			while (Endpoint_BytesInEndpoint())
+			{			
+				/* Test received bytes, looking for 0 to turn off the LED */
+				/* and 1 to turn on the LED, others values are ignored */
+				ucData = Endpoint_Read_Byte();
+				switch(ucData)
+				{
+					case '0':
+					PORTD &= ~(1<<PD4); /* Turn on LED */
+					break;
+						
+					case '1':	
+					PORTD |= (1<<PD4); /* Turn off LED */
+					break;
+						
+					default: /* Do nothing with other values */
+					break;
+				}
+			}
+			
+			/* Clear the endpoint buffer */
+			Endpoint_ClearCurrentBank();
+			
+			/* Select the Serial Tx Endpoint */
+			Endpoint_SelectEndpoint(CDC_TX_EPNUM);
+
+			/* Wait until Serial Tx Endpoint Ready for Read/Write */
+			while (!(Endpoint_ReadWriteAllowed()));
+			
+			/* Check before sending the data if the endpoint is completely full */
+			bool IsFull = (Endpoint_BytesInEndpoint() == CDC_TXRX_EPSIZE);
+			
+			/* Send back the same value by USB */
+			Endpoint_Write_Byte(ucData);
+			
+			/* Send the data */
+			Endpoint_ClearCurrentBank();
+
+			/* If a full endpoint was sent, we need to send an empty packet afterwards to terminate the transfer */
+			if (IsFull)
+			{
+				/* Wait until Serial Tx Endpoint Ready for Read/Write */
+				while (!(Endpoint_ReadWriteAllowed()));
+
+				/* Send an empty packet to terminate the transfer */
+				Endpoint_ClearCurrentBank();
+			}
+		}
+	}
+}
+
+void Hardware_Init(void)
+{
+	DDRD |= (1<<PD4); /* Output pin for the LED */
+}
