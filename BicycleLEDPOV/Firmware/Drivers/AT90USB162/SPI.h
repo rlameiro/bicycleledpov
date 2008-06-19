73859af1acb7abc2076aec58e5019fbeb9cb0ca3
casainho <casainho@a02c272c-b14d-0410-965c-4f7f0f620247>
2008-06-19 19:39:40 +0000

    add files from Firmware -- add ddrivers for SPI

---
+++ BicycleLEDPOV/Firmware/Drivers/AT90USB162/SPI.h
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+
+/*******************************************************************************
+When the DORD bit is written to one, the LSB of the data word is
+transmitted first. When the DORD bit is written to zero, the MSB of the data
+word is transmitted first. */
+#define DATA_ORDER_MSB	  0
+#define DATA_ORDER_LSB	  1
+
+void SPI_MasterInit(unsigned char DataOrder);
+void SPI_Disable(void);
+void SPI_MasterTransmit(unsigned char cData);
+unsigned char SPI_MasterReceive(void);
