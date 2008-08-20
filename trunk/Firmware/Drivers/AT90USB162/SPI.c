25e43507e96c6b9888af38f2f48e03b572ed5b8e
casainho <casainho@a02c272c-b14d-0410-965c-4f7f0f620247>
2008-08-20 11:20:41 +0000

    New try... -- Ops, did deleted all the trunk directory before, so I did checkout the revision before and I am commiting now, with that file created by Herberto, removed.

---
+++ trunk/Firmware/Drivers/AT90USB162/SPI.c
@@ -0,0 +1,58 @@
+/*
+             Bicycle LED POV
+     Copyright (C) Jorge Pinto aka Casainho, 2008.
+              
+  casainho [at] gmail [dot] com
+      www.casainho.net
+
+ Released under the GPL Licence, Version 3       
+*/
+
+#include "SPI.h"
+
+void SPI_MasterInit(unsigned char ucDataOrder)
+{
+	/* Put I/Os as outputs for SPI MOSI and SCK */
+	DDRB	|=  ((1<<PB0) /* SPI_SS - this line should be input to SPI works ok */
+			|   (1<<PB1) /* SPI_SCK */
+			|   (1<<PB2)); /* SPI_MOSI */
+	
+	/* Disable the Power Reduction on Serial Peripheral Interface */
+	PRR0 &= ~(1<<PRSPI);
+	
+	/* Enable SPI, Master, set clock rate fck/2 */
+	SPCR |= ((1<<SPE) | (1<<MSTR) | (1<<SPI2X));
+
+	/* Configure the Data Order of first be to be transmited */
+	if (ucDataOrder)
+		SPCR |= (1<<DORD);
+}
+
+void SPI_Disable(void)
+{
+	/* Disable SPI */
+	SPCR &= ~(1<<SPE);
+	
+	/* Enable the Power Reduction on Serial Peripheral Interface */
+	PRR0 |= (1<<PRSPI);
+	
+	/* Put I/Os as inputs for SPI MOSI and SCK */
+	DDRB	&=  ~((1<<PB1) /* SPI_SCK */
+			|   (1<<PB2)); /* SPI_MOSI */
+}
+
+void SPI_MasterTransmit(unsigned char ucData)
+{
+	/* Start transmission */
+	SPDR = ucData;
+	
+	/* Wait for transmission complete */
+	while(bit_is_clear(SPSR,SPIF))
+		;
+}
+
+unsigned char SPI_MasterReceive(void)
+{
+  /* Return Data Register */
+  return SPDR;
+}
