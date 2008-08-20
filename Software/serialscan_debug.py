ee5403cd1b70de6ddfa1b85238c291327768cf75
casainho <casainho@a02c272c-b14d-0410-965c-4f7f0f620247>
2008-08-20 11:42:55 +0000

    Commit all Software directory after the last mistake.

---
+++ Software/serialscan_debug.py
@@ -0,0 +1,41 @@
+import os
+import serial
+import string
+
+
+
+lista = []
+lista = os.listdir('/dev/')
+
+serial_ls =[]
+
+for item in lista:
+    a = item[0:3]
+    if a == "tty":
+        serial_ls.append(item)
+    else:
+        pass
+
+#print lista
+
+
+
+#print serial_ls
+
+def scan():
+    """scan for available ports. return a list of tuples (num, name)"""
+    available = []
+    for item in serial_ls:
+        try:
+            s = serial.Serial("/dev/"+item)
+            available.append(s.portstr)
+            s.close()   
+        except serial.SerialException:
+            pass
+    return available
+    
+print scan()
+
+a = len(scan())
+
+print "\n O tamanho da lista e'="+ str(a) + "\n"
