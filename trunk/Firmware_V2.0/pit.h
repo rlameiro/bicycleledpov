ab8cdb2b8f3783c6f5a63fb550995e72525d79f1
casainho <casainho@a02c272c-b14d-0410-965c-4f7f0f620247>
2010-05-15 23:20:56 +0000

    Added first firmware files for V2.0.

---
+++ trunk/Firmware_V2.0/pit.h
@@ -0,0 +1,17 @@
+/*
+ * BicycLED POV
+ *
+ * Copyright (C) Jorge Pinto aka Casainho, 2010.
+ *
+ *   casainho [at] gmail [dot] com
+ *
+ *
+ * Released under the GPL Licence, Version 3
+ */
+
+/* Global variables */
+
+
+void timer0_irq_handler (void);
+void timer0_setup (void);
+void delay (long ms);
